eb105d335bcf00d650a860792115f951b6289042
luke2327 <qwepoi8526@gmail.com>
2024-04-16 09:47:40 +0900

    유틸리티 타입 추가

---
+++ common/utilityTypes.ts
@@ -0,0 +1,13 @@
+export type DeepPartial<T> = {
+  [P in keyof T]?: DeepPartial<T[P]>;
+};
+
+export type EntityCondition<T> = {
+  [P in keyof T]?: T[P] | T[P][] | undefined;
+};
+
+export type MaybeType<T> = T | undefined;
+
+export type NullableType<T> = T | null;
+
+export type OrNeverType<T> = T | never;
